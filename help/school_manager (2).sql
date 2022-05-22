1fe47dbbb0c85ac1ebcba7088cec0e29e2c148fe
ivanmasaba <ivanmasaba@gmail.com>
2022-05-22 20:31:26 +0300

    project update

--- help/school_manager (2).sql
+++ help/school_manager (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2022 at 06:35 PM
+-- Generation Time: May 22, 2022 at 07:29 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -24,19 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `appointments`
---
-
-CREATE TABLE `appointments` (
-  `id` int(11) NOT NULL,
-  `when` date NOT NULL,
-  `content` text NOT NULL,
-  `operation` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `login`
 --
 
@@ -53,8 +40,14 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`reg_num`, `uname`, `password`, `level`, `change_details`) VALUES
+(1, 'ivan', '9999', 'teacher', ''),
 (2, 'andy', '4321', 'admin', ''),
-(3, 'mark', '1000', 'student', 'PERMITED');
+(3, 'mark', '1000', 'student', 'PERMITED'),
+(4, 'musamali', '1', 'student', 'PERMITED'),
+(5, 'john', '111', 'student', ''),
+(6, 'fred', '3', 'student', ''),
+(7, 'benard', '4', 'student', ''),
+(8, 'william', '2', 'student', '');
 
 -- --------------------------------------------------------
 
@@ -63,6 +56,7 @@ INSERT INTO `login` (`reg_num`, `uname`, `password`, `level`, `change_details`) 
 --
 
 CREATE TABLE `marks` (
+  `id` int(11) NOT NULL,
   `reg_num` int(11) NOT NULL,
   `subj_id` int(30) NOT NULL,
   `test_score` int(11) NOT NULL,
@@ -75,14 +69,42 @@ CREATE TABLE `marks` (
 -- Dumping data for table `marks`
 --
 
-INSERT INTO `marks` (`reg_num`, `subj_id`, `test_score`, `exam_score`, `total_score`, `grade`) VALUES
-(3, 1, 40, 45, 85, 'B+'),
-(3, 2, 35, 35, 70, 'C'),
-(3, 3, 40, 45, 85, 'B+'),
-(3, 4, 20, 48, 68, 'C-'),
-(3, 5, 33, 40, 73, 'C'),
-(3, 6, 42, 42, 84, 'B'),
-(3, 7, 40, 48, 88, 'B+');
+INSERT INTO `marks` (`id`, `reg_num`, `subj_id`, `test_score`, `exam_score`, `total_score`, `grade`) VALUES
+(1, 3, 1, 48, 45, 93, 'B+'),
+(2, 3, 2, 35, 35, 70, 'C'),
+(3, 3, 3, 40, 45, 85, 'B+'),
+(4, 3, 4, 20, 48, 68, 'C-'),
+(5, 3, 5, 33, 40, 73, 'C'),
+(6, 3, 6, 42, 42, 84, 'B'),
+(7, 3, 7, 40, 48, 88, 'B+'),
+(8, 4, 1, 28, 44, 72, 'C'),
+(9, 4, 2, 23, 44, 67, 'F'),
+(10, 4, 3, 23, 44, 67, 'F'),
+(11, 4, 4, 23, 44, 67, 'C-'),
+(12, 4, 5, 23, 44, 67, 'B'),
+(13, 4, 6, 23, 44, 67, 'B'),
+(14, 4, 7, 23, 44, 67, 'A'),
+(16, 6, 1, 0, 0, 0, 'F'),
+(17, 6, 2, 0, 0, 0, 'F'),
+(18, 6, 3, 0, 0, 0, 'F'),
+(19, 6, 4, 0, 0, 0, 'F'),
+(20, 6, 5, 0, 0, 0, 'F'),
+(21, 6, 6, 0, 0, 0, 'F'),
+(22, 6, 7, 0, 0, 0, 'F'),
+(23, 7, 1, 0, 0, 0, 'F'),
+(24, 7, 2, 0, 0, 0, 'F'),
+(25, 7, 3, 0, 0, 0, 'F'),
+(26, 7, 4, 0, 0, 0, 'F'),
+(27, 7, 5, 0, 0, 0, 'F'),
+(28, 7, 6, 0, 0, 0, 'F'),
+(29, 7, 7, 0, 0, 0, 'F'),
+(30, 8, 1, 50, 50, 100, 'F'),
+(31, 8, 2, 0, 0, 0, 'F'),
+(32, 8, 3, 0, 0, 0, 'F'),
+(33, 8, 4, 0, 0, 0, 'F'),
+(34, 8, 5, 0, 0, 0, 'F'),
+(35, 8, 6, 0, 0, 0, 'F'),
+(36, 8, 7, 0, 0, 0, 'F');
 
 -- --------------------------------------------------------
 
@@ -91,6 +113,7 @@ INSERT INTO `marks` (`reg_num`, `subj_id`, `test_score`, `exam_score`, `total_sc
 --
 
 CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
   `reg_num` int(11) NOT NULL,
   `fname` varchar(50) NOT NULL,
   `sname` varchar(50) NOT NULL,
@@ -111,9 +134,13 @@ CREATE TABLE `registration` (
 -- Dumping data for table `registration`
 --
 
-INSERT INTO `registration` (`reg_num`, `fname`, `sname`, `class`, `stream`, `birth_date`, `email`, `fathers_name`, `mothers_name`, `parent_number`, `parent_address`, `illness`, `disability`, `others`) VALUES
-(3, 'mark', 'joel', 'S1', 'a', '1994-06-16', 'nondabashir@gmail.com', 'musimbi', 'steven', '0788720915', 'kampala', 'no', 'no', 'no'),
-(3, 'mark', 'joel', 'S1', 'a', '1994-06-16', 'nondabashir@gmail.com', 'musimbi', 'steven', '0788720915', 'kampala', 'no', 'no', 'no');
+INSERT INTO `registration` (`id`, `reg_num`, `fname`, `sname`, `class`, `stream`, `birth_date`, `email`, `fathers_name`, `mothers_name`, `parent_number`, `parent_address`, `illness`, `disability`, `others`) VALUES
+(1, 3, 'mark', 'joel', 'senior one', 'a', '1994-06-16', 'nondabashir@gmail.com', 'musimbi', 'steven', '0788720915', 'kampala', 'no', 'no', 'no'),
+(3, 4, 'musamali', 'derrick', 'senior one', 'a', '2022-05-12', 'derick@yahoo.com', 'musa', 'esther', '0771380954', 'mbale', 'no', 'no', 'no'),
+(4, 5, 'john', 'godfrey', 'senior three', 'B', '2016-06-29', 'nondabashir@gmail.com', 'henry', 'esther', '0702049096', 'kampala', 'no', 'no', 'no'),
+(5, 8, 'william', 'isaac', 'senior one', 'A', '2022-05-10', 'will@hotmail.com', 'henry', 'steven', '0788720915', 'kampala', 'no', 'no', 'no'),
+(6, 6, 'fred', 'bill', 'senior one', 'A', '2022-06-01', 'andymasso@yahoo.com', 'musimbi', 'esther', '0702049096', 'kampala', 'no', 'no', 'no'),
+(7, 7, 'benard', 'gregory', 'senior one', 'A', '2022-05-04', 'ben@yahoo.com', 'henry', 'steven', '0702049096', 'kampala', 'no', 'no', 'no');
 
 -- --------------------------------------------------------
 
@@ -123,21 +150,23 @@ INSERT INTO `registration` (`reg_num`, `fname`, `sname`, `class`, `stream`, `bir
 
 CREATE TABLE `staff` (
   `id` int(11) NOT NULL,
-  `staff_id` varchar(20) NOT NULL,
-  `fname` varchar(50) NOT NULL,
-  `sname` varchar(50) NOT NULL,
+  `staff_id` varchar(30) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `subj_id` int(11) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `sname` varchar(30) NOT NULL,
   `birth_date` date NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `parent_number` varchar(30) NOT NULL,
-  `parent_address` varchar(30) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `staff`
 --
 
-INSERT INTO `staff` (`id`, `staff_id`, `fname`, `sname`, `birth_date`, `email`, `parent_number`, `parent_address`) VALUES
-(1, 'andy', 'andy', 'masso', '2022-05-06', 'andymasso@yahoo.com', '0702049096', 'kampala');
+INSERT INTO `staff` (`id`, `staff_id`, `class_id`, `subj_id`, `fname`, `sname`, `birth_date`, `email`, `phone`, `address`) VALUES
+(12, 'ivan', 1, 1, 'ivan', 'masaba', '2010-10-05', 'ivanmasaba@gmail.com', '0778465853', 'kampala');
 
 -- --------------------------------------------------------
 
@@ -168,12 +197,6 @@ INSERT INTO `subjects` (`id`, `subj_name`) VALUES
 --
 
 --
--- Indexes for table `appointments`
---
-ALTER TABLE `appointments`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
@@ -183,7 +206,14 @@ ALTER TABLE `login`
 -- Indexes for table `marks`
 --
 ALTER TABLE `marks`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `subj_id` (`subj_id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `staff`
@@ -202,22 +232,28 @@ ALTER TABLE `subjects`
 --
 
 --
--- AUTO_INCREMENT for table `appointments`
---
-ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `reg_num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `reg_num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `marks`
+--
+ALTER TABLE `marks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `subjects`
